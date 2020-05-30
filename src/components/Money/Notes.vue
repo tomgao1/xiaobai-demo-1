2be20a181e5afb4a1c9edc7c6c9818befb39502f
tomgao <406038564@qq.com>
2020-05-30 18:11:28 +0800

    使用v-model代替:value和@input绑定

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -2,9 +2,9 @@
     <div>
 <label class="notes">
     <span class="name">备注</span>
-    <input type="text" :value="value"
-                        @input ="onInput"
-                         placeholder="在这里输入备注">
+    <input type="text" 
+                      v-model= "value"
+                     placeholder="在这里输入备注">
    </label>
     </div>
 </template>
