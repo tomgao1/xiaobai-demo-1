f231d442eb9c99ada5c26062b6babf0c4eb1a40d
tomgao <406038564@qq.com>
2020-05-31 15:39:52 +0800

    添加createAt字段

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -58,7 +58,8 @@
         }
         ok(){
           this.$emit('update:value',this.output);
-          this.$emit('submit',this.output)
+          this.$emit('submit',this.output);
+          this.output = '0';
         }
     }
 
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout class-prefix="layout">
-    {{record}}
+    {{recordList}}
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
     <Notes @update:value="onUpdateNotes"/>
@@ -16,12 +16,14 @@
   import Notes from '@/components/Money/Notes.vue';
   import Tags from '@/components/Money/Tags.vue';
   import {Component, Watch} from 'vue-property-decorator';
-
+   
+   const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
      type Record = {
        tags: string[];
        notes: string;
        type: string;
-       amount: number;
+       amount: number;//数据类型object |string
+       createAt?: Date; //类/购值函数
      }
        @Component ({
          components: {Tags,Notes,Types,NumberPad}
@@ -29,7 +31,7 @@
 
       export default class Money extends Vue{
         tags = ['衣','食','住','行','彩票'];
-        recordList: Record[] = [];
+        recordList: Record[] = recordList;
         record: Record = {
           tags:[],notes:'',type:'-',amount:0
         };
@@ -40,14 +42,15 @@
             this.record.notes = value;
           }
           saveRecord(){
-            const record2 = JSON.parse(JSON.stringify(this.record));
-            this.recordList.push(record2);
-            console.log(this.recordList);
+          const record2: Record = JSON.parse(JSON.stringify(this.record));
+           record2.createAt = new Date()
+           this.recordList.push(record2);
+            
           }
           @Watch('recordList')
           onRecordListChange(){
             window.localStorage.setItem('recordList',JSON.stringify(this.recordList));
-            
+
           }
 
       }
