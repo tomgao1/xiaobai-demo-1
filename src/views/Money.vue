ed22de4722260f6b845c12a60d445abc034bcd13
tomgao <406038564@qq.com>
2020-06-02 10:09:56 +0800

    将create封装到recordListModel

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -43,15 +43,12 @@
             this.record.notes = value;
           }
           saveRecord(){
-          const record2: RecordItem = recordListModel.clone(this.record);
-      
-          record2.createAt = new Date()
-           this.recordList.push(record2);
+         recordListModel.create(this.record);
             
           }
           @Watch('recordList')
           onRecordListChange(){
-            recordListModel.save(this.recordList);
+            recordListModel.save();
 
           }
 
