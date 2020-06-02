1e93d6db6ea86288fa72e17db8568fd74b995117
tomgao <406038564@qq.com>
2020-06-02 15:12:11 +0800

    封装record的操作

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -6,6 +6,7 @@ const recordListModel ={
     const record2: RecordItem = clone(record);
     record2.createAt =new Date();
     this.data.push(record2);
+    this.save();
   },
  
   fetch(){
@@ -14,8 +15,8 @@ const recordListModel ={
   },
 
 save(){
-  window.localStorage.setItem(localStorageKeyName,
-    JSON.stringify(this.data));
+  window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
+
 }
 }
 export default recordListModel;
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -19,11 +19,9 @@
   import Types from '@/components/Money/Types.vue';
   import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
-  import {Component, Watch} from 'vue-property-decorator';
-  import recordListModel from '@/models/recordListModel';
+  import {Component} from 'vue-property-decorator';
+ 
 
-  const recordList: RecordItem[] = recordListModel.fetch();
-  
   
        @Component ({
          components: {Tags,FormItem,Types,NumberPad}
@@ -32,7 +30,7 @@
       export default class Money extends Vue{
         tags = window.tagList;
 
-        recordList: RecordItem[] = recordList;
+        recordList = window.recordList;
         record: RecordItem = {
           tags:[],notes:'',type:'-',amount:0
         };
@@ -43,13 +41,7 @@
             this.record.notes = value;
           }
           saveRecord(){
-         recordListModel.create(this.record);
-            
-          }
-          @Watch('recordList')
-          onRecordListChange(){
-            recordListModel.save();
-
+        window.createRecord(this.record);
           }
 
       }
