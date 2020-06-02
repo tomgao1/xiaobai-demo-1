ecb4a9d79be7f5660234aff9f7fc1f51a6040113
tomgao <406038564@qq.com>
2020-06-02 15:34:52 +0800

    将所有数据操作封装在store

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -20,7 +20,7 @@
   import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
   import {Component} from 'vue-property-decorator';
- 
+  import store from '@/store/index2.ts';
 
   
        @Component ({
@@ -28,9 +28,8 @@
        })
 
       export default class Money extends Vue{
-        tags = window.tagList;
-
-        recordList = window.recordList;
+        tags = store.tagList;
+        recordList = store.recordList;
         record: RecordItem = {
           tags:[],notes:'',type:'-',amount:0
         };
@@ -41,7 +40,7 @@
             this.record.notes = value;
           }
           saveRecord(){
-        window.createRecord(this.record);
+        store.createRecord(this.record);
           }
 
       }
