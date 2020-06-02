ced11ad5ddd82e508fc1137821a524ef13168a12
tomgao <406038564@qq.com>
2020-06-02 10:02:43 +0800

    重新封装recordlistmodel

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,14 +1,17 @@
 const localStorageKeyName = 'recordList'
 const recordListModel ={
+  data:[] as RecordItem[],
   clone(data: RecordItem[] |RecordItem){
     return JSON.parse(JSON.stringify(data));
   },
   fetch(){
-    return JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[];
+    this.data = JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[];
+       return this.data;
   },
 
-save(data: RecordItem[]){
-    window.localStorage.setItem('recordList',JSON.stringify(data))
+save(){
+  window.localStorage.setItem(localStorageKeyName,
+    JSON.stringify(this.data));
 }
 }
 export default recordListModel;
