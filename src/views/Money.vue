03ca2663ba06f9441c1743bf7415bcbf9aa1521a
tomgao <406038564@qq.com>
2020-07-15 16:08:26 +0800

    添加 createdAt 输入框

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,7 +2,14 @@
   <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Tabs :data-source="recordTypeList"
-          :value.sync="record.type"/>
+         :value.sync="record.type"/>
+    <div class="createdAt">
+      <FormItem field-name="日期"
+        type="date"
+        placeholder="在这里输入日期"
+        :value.sync="record.createdAt"
+      />
+    </div>
     <div class="notes">
       <FormItem field-name="备注"
                 placeholder="在这里输入备注"
@@ -35,7 +42,7 @@
     }
      recordTypeList = recordTypeList;
         record: RecordItem = {
-          tags:[],notes:'',type:'-',amount:0
+          tags:[],notes:'',type:'-',amount:0, createdAt: new Date().toISOString()
         };
          created(){
             this.$store.commit('fetchRecords');
