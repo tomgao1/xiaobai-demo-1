71e77f3a7d35e266cead1054193b4bf6604e289a
tomgao <406038564@qq.com>
2020-07-15 20:24:05 +0800

    自己封装chart组件，完善其样式

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,6 +1,9 @@
 <template>
   <Layout>
   <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
+ <div class="chart-wrapper" ref="chartWrapper">
+   <Chart class="chart" :options="x"/>
+ </div>
  <ol v-if="groupedList.length>0">
   <li v-for="(group, index) in groupedList" :key="index">
         <h3 class="title">{{beautify(group.title)}} <span>￥{{group.total}}</span></h3>
@@ -30,15 +33,21 @@
   import recordTypeList from '@/constants/recordTypeList';
   import dayjs from 'dayjs';
   import clone from '@/lib/clone';
+  import Chart from '../components/Chart.vue';
+
 
 
   @Component({
-    components:{Tabs},
+    components:{Tabs,Chart},
   })
  export default class Statistics extends Vue {
     tagString(tags: Tag[]) {
       return tags.length === 0 ? '无' : tags.map(t=>t.name).join('，');
     }
+    mounted() {
+      (this.$refs.chartWrapper as HTMLDivElement).scrollLeft = 9999;
+    }
+
      beautify(string: string) {
        const day = dayjs(string);
        const now = dayjs();
@@ -54,6 +63,47 @@
          return day.format('YYYY年M月D日');
        }
      }
+     get x() {
+      return {
+       grid: {
+          left: 0,
+          right: 0,
+        },
+
+        xAxis: {
+          type: 'category',
+          data: [
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+            '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+            '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
+          ],
+          axisTick: {alignWithLabel: true},
+          axisLine: {lineStyle: {color: '#0099FF'}}
+        },
+
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+           symbol: 'circle',
+          symbolSize: 12,
+          itemStyle: {borderWidth: 1, color: '#E6795D', borderColor: '#666'},
+          // lineStyle: {width: 10},
+          data: [
+            820, 932, 901, 934, 1290, 1330, 1320,
+            820, 932, 901, 934, 1290, 1330, 1320,
+            820, 932, 901, 934, 1290, 1330, 1320,
+            820, 932, 901, 934, 1290, 1330, 1320, 1, 2
+          ],
+          type: 'line'
+        }],
+        tooltip: {
+          show: true, triggerOn: 'click',
+          position: 'top',
+          formatter: '{c}'
+        }
+      };
+    }
 
     get recordList() {
       return (this.$store.state as RootState).recordList;
@@ -94,14 +144,17 @@
 
     type = '-';
     recordTypeList = recordTypeList;
-      
-
+  
   }
 
 
 </script>
 
 <style lang="scss" scoped>
+.echarts {
+    max-width: 100%;
+    height: 400px;
+  }
  
   ::v-deep {
     .type-tabs-item {
@@ -142,5 +195,13 @@
   padding: 16px;
   text-align: center;
 }
-
+.chart {
+    width: 430%;
+    &-wrapper {
+      overflow: auto;
+      &::-webkit-scrollbar {
+        display: none;
+      }
+    }
+  }
 </style>
