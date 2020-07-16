be937558a7a50f12d90f1808deaf04d81965bcf9
tomgao <406038564@qq.com>
2020-07-16 14:36:32 +0800

    构造30天数据

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -34,6 +34,8 @@
   import dayjs from 'dayjs';
   import clone from '@/lib/clone';
   import Chart from '../components/Chart.vue';
+  import _ from 'lodash';
+  import day from 'dayjs';
 
 
 
@@ -46,7 +48,7 @@
     }
     mounted() {
      const div = (this.$refs.chartWrapper as HTMLDivElement);
-      console.log(div.scrollWidth);
+      
       div.scrollLeft = div.scrollWidth;
     }
      beautify(string: string) {
@@ -64,7 +66,34 @@
          return day.format('YYYY年M月D日');
        }
      }
+     get y() {
+      const today = new Date();
+      const array = [];
+      for (let i = 0; i <= 29; i++) {
+        // this.recordList = [{date:7.3, value:100}, {date:7.2, value:200}]
+        const dateString = day(today)
+          .subtract(i, 'day').format('YYYY-MM-DD');
+        const found = _.find(this.recordList, {
+          createdAt: dateString
+        });
+        array.push({
+          date: dateString, value: found ? found.amount : 0
+        });
+      }
+      array.sort((a, b) => {
+        if (a.date > b.date) {
+          return 1;
+        } else if (a.date === b.date) {
+          return 0;
+        } else {
+          return -1;
+        }
+      });
+      return array;
+    }
      get x() {
+       const keys = this.y.map(item => item.date);
+       const values = this.y.map(item =>item.value);
       return {
        grid: {
           left: 0,
@@ -73,11 +102,7 @@
 
         xAxis: {
           type: 'category',
-          data: [
-            '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
-            '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
-            '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
-          ],
+         date:keys,
           axisTick: {alignWithLabel: true},
           axisLine: {lineStyle: {color: '#0099FF'}}
         },
@@ -90,12 +115,7 @@
           symbolSize: 12,
           itemStyle: {borderWidth: 1, color: '#E6795D', borderColor: '#666'},
           // lineStyle: {width: 10},
-          data: [
-            820, 932, 901, 934, 1290, 1330, 1320,
-            820, 932, 901, 934, 1290, 1330, 1320,
-            820, 932, 901, 934, 1290, 1330, 1320,
-            820, 932, 901, 934, 1290, 1330, 1320, 1, 2
-          ],
+          data: values,
           type: 'line'
         }],
         tooltip: {
@@ -130,8 +150,6 @@
        }
       result.map(group => {
         group.total = group.items.reduce((sum, item) => {
-          console.log(sum);
-          console.log(item);
           return sum + item.amount;
         }, 0);
       });
